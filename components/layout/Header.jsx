23d33e1369bab4ad1301d392729af8a60bd3834c
jajorgbor <joshuaajogbor@gmail.com>
2024-05-05 17:44:55 +0100

    Imroved header UI

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -6,59 +6,109 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
 } from "@nextui-org/react";
+import { Sling as HamburgerButton } from "hamburger-react";
 import Link from "next/link";
-import { ShoppingCart, Search } from "react-feather";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { ArrowRight, Search, ShoppingCart } from "react-feather";
 
 export default function Header() {
+  const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [expandSearchField, setExpandSearchField] = useState(false);
+  const menu = [
+    { label: "Home", route: "/" },
+    { label: "Shop", route: "/shop" },
+    { label: "Contact", route: "/contact" },
+    { label: "Categories", route: "/categories" },
+    { label: "Products", route: "/products" },
+  ];
+
   return (
     <Navbar
-      className="md:px-[5%] py-3 max-w-screen-xl mx-auto"
+      className="lg:px-4 py-3 max-w-screen-xl mx-auto z-50 backdrop-blur-sm bg-[rgba(255,255,255,0.6)]"
       position="sticky"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarBrand className="sm:hidden">
-        <Logo />
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4 " justify="start">
-        <NavbarBrand>
+      <NavbarContent>
+        <NavbarItem className="lg:hidden">
+          <HamburgerButton
+            size={25}
+            onToggle={(toggled) => setIsMenuOpen(toggled)}
+            duration={0.8}
+            color="gray"
+          />
+        </NavbarItem>
+        <NavbarBrand className="lg:hidden">
+          <Logo
+            className={`${
+              expandSearchField ? "text-sm md:text-2xl" : ""
+            } transition-all`}
+          />
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden lg:flex gap-4 " justify="start">
+        <NavbarBrand className="mr-5">
           <Logo />
         </NavbarBrand>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Home
-          </Link>
+          <Link className="text-foreground hover:text-primary" href="#"></Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Shop
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Contact
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Pricing
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Product
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Input
-            type="search"
-            placeholder="Search Store"
-            endContent={<Search />}
-          />
-        </NavbarItem>
+        {menu.map((item, index) => (
+          <NavbarItem isActive={item.route == router.pathname} key={index}>
+            <Link
+              href={item.route}
+              aria-current="page"
+              className="text-foreground hover:text-primary"
+            >
+              {item.label}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+        <NavbarItem>
+          <div
+            className={` transition-all w-[38px] ${
+              expandSearchField ? "w-[180px]" : ""
+            } md:w-[250px] overflow-hidden  rounded-xl flex`}
+          >
+            <button
+              type="button"
+              onClick={() => setExpandSearchField(!expandSearchField)}
+              className={`px-2 rounded-l-xl bg-primary  border border-primary text-white md:hidden`}
+            >
+              {!expandSearchField ? (
+                <Search className="  md:hidden" size={20} />
+              ) : (
+                <ArrowRight className=" md:hidden" size={20} />
+              )}
+            </button>
+            <Input
+              type="search"
+              placeholder="Search Store"
+              className="min-w-[200px] rounded-l-none"
+              startContent={
+                <Search className=" hidden md:inline-block " size={20} />
+              }
+            />
+          </div>
+        </NavbarItem>
+        <NavbarItem className=" ">
+          <Button
+            href="#"
+            as={Link}
+            size="sm"
+            variant="light"
+            color="primary"
+            className="font-bold"
+          >
+            Login
+          </Button>
         </NavbarItem>
         <NavbarItem>
           <Button
@@ -76,6 +126,22 @@ export default function Header() {
           <ShoppingCart size={20} />
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu className="h-[500px] fixed top-[4.5rem] lg:hidden w-full backdrop-blur-sm bg-[rgba(255,255,255,0.6)]">
+        {menu.map((item, index) => (
+          <NavbarMenuItem
+            key={index}
+            className={`transition-all  transform hover:translate-x-5 hover:font-bold hover:text-white hover:bg-primary px-3 py-1 rounded-lg duration-200 ${
+              item.route == router.pathname
+                ? "text-primary font-bold bg-foreground-200 translate-x-5"
+                : ""
+            }`}
+          >
+            <Link href={item.route} className="inline-block w-full">
+              {item.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
